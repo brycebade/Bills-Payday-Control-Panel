e344f799f5c757deca5fccdf6e9688ce0d1d2060
brycebade <bryce.bade@gmail.com>
2025-12-09 15:55:50 -0600

    Replace billDue with comprehensive bills list

--- data.js
+++ data.js
@@ -1,9 +1,144 @@
 /* Object of Bills */
 
-const billDue = [
-    {
-        name: "Evergy",
-        amountOwed: 148.97,
-        paid: false
-    },
-]
+const bills = [
+  {
+    name: "Verizon",
+    amountOwed: 70.0,
+    dueDay: 1,
+    paid: false
+  },
+  {
+    name: "Menard's",
+    amountOwed: 64.0,
+    dueDay: 5,
+    paid: false,
+    paymentsRemaining: 41
+  },
+  {
+    name: "Philips 66",
+    amountOwed: 29.00,
+    dueDay: 8,
+    paid: false
+  },
+  {
+    name: "Discover",
+    amountOwed: 73.00,
+    dueDay: 8,
+    paid: false
+  },
+  {
+    name: "Home Depot",
+    amountOwed: 48.00,
+    dueDay: 9,
+    paid: false,
+    paymentsRemaining: 5
+  },
+  {
+    name: "Storage",
+    amountOwed: 39.00,
+    dueDay: 10,
+    paid: false
+  },
+  {
+    name: "Capital One QS - 3553",
+    amountOwed: 28.00,
+    dueDay: 28,
+    paid: false
+  },
+  {
+    name: "Affirm #1",
+    amountOwed: 16.09,
+    dueDay: 16,
+    paid: false,
+    paymentsRemaining: 4
+  },
+  {
+    name: "Affirm #2",
+    amountOwed: 22.79,
+    dueDay: 19,
+    paid: false,
+    paymentsRemaining: 15
+  },
+  {
+    name: "Affirm #3",
+    amountOwed: 21.36,
+    dueDay: 28,
+    paid: false,
+    paymentsRemaining: 5
+  },
+  {
+    name: "Affirm #4",
+    amountOwed: 28.21,
+    dueDay: 3,
+    paid: false,
+    paymentsRemaining: 18
+  },
+  {
+    name: "Affirm #5",
+    amountOwed: 17.01,
+    dueDay: 8,
+    paid: false,
+    paymentsRemaining: 12
+  },
+  {
+    name: "Nebraska Furniture Mart",
+    amountOwed: 120.00,
+    dueDay: 15,
+    paid: false,
+    paymentsRemaining: 5
+  },
+  {
+    name: "Socket",
+    amountOwed: 75.00,
+    dueDay: 20,
+    paid: false
+  },
+  {
+    name: "Evergy",
+    amountOwed: 150.00,
+    dueDay: 20,
+    paid: false
+  },
+  {
+    name: "Chase",
+    amountOwed: 175.00,
+    dueDay: 20,
+    paid: false
+  },
+  {
+    name: "Merrick",
+    amountOwed: 67.00,
+    dueDay: 20,
+    paid: false
+  },
+  {
+    name: "Apple One",
+    amountOwed: 20.00,
+    dueDay: 24,
+    paid: false
+  },
+  {
+    name: "Capital One Platinum",
+    amountOwed: 95.00,
+    dueDay: 28,
+    paid: false
+  },
+  {
+    name: "Capital One QS - 5859",
+    amountOwed: 38.00,
+    dueDay: 11,
+    paid: false
+  },
+  {
+    name: "Audible",
+    amountOwed: 15.00,
+    dueDay: 29,
+    paid: false
+  },
+  {
+    name: "Progressive",
+    amountOwed: 160.00,
+    dueDay: 30,
+    paid: false
+  }
+];
