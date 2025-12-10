ea8a38158a51640cb4caefd89dcd9e2bab5cfc1f
brycebade <bryce.bade@gmail.com>
2025-12-10 06:40:17 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,24 @@ const paycheckBtn = document.querySelector("#set-paycheck")
 const totalBillsAmnt = document.querySelector("#total-bills")
 const totalPaidAmnt = document.querySelector("#total-paid")
 const remainingAmnt = document.querySelector("#remaining")
+const billsTbody = document.querySelector("#bills-tbody")
 
 let paycheckAmount = 0
+
+const renderBills = () => {
+   billsTbody.innerHTML = ""
+   
+   for (const bill of bills) {
+    const topRow = document.createElement("tr")
+    const billName = document.createElement("td")
+    billName.textContent = bill.name
+    topRow.appendChild(billName)
+    const billAmount = document.createElement("td")
+    billAmount.textContent = `$${bill.amountOwed.toFixed(2)}`
+    topRow.appendChild(billAmount)
+    billsTbody.appendChild(topRow)
+   }
+}
 
 paycheckBtn.addEventListener("click", () => {
     const checkAmount = Number(paycheckInput.value)
@@ -15,4 +31,4 @@ paycheckBtn.addEventListener("click", () => {
     remainingAmnt.textContent = `Remaining: $${paycheckAmount.toFixed(2)}`
 })
 
-console.log(bills)
+renderBills()
